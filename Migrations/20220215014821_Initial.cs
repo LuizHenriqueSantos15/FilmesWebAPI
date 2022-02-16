94f892085f761526b29ff4ea64f2eacd634c8179
Luiz Henrique Santos <96305078+LuizHenriqueSantos15@users.noreply.github.com>
2022-02-16 17:17:34 -0300

    Add files via upload

---
+++ Migrations/20220215014821_Initial.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LojaRoupaAPIWeb.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Filmes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Produtora = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UrlBanner = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UrlConteudo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Descrição = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Filmes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Filmes");
+        }
+    }
+}
