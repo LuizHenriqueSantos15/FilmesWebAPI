f210cc58f8076c6784856e259d8a1390792f0c64
Luiz Henrique Santos <96305078+LuizHenriqueSantos15@users.noreply.github.com>
2022-02-16 17:18:45 -0300

    Add files via upload

---
+++ Models/Filme.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaRoupaAPIWeb.Models
+{
+    public class Filme
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Produtora { get; set; }
+        public string UrlBanner { get; set; }
+        public string UrlConteudo { get; set; }
+        public string Descrição { get; set; }
+    }
+}
---
+++ Models/FilmeContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaRoupaAPIWeb.Models
+{
+    public class FilmeContext : DbContext
+    {
+        public DbSet<Filme> Filmes { get; set; }
+
+        public FilmeContext()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-A86BJI0;Initial Catalog=FilmeSerie;User ID=sa;Password=1234");
+        }
+    }
+}
