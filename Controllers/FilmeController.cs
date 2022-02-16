9e4fff30550e41af27695877a0db35f1b93cc799
Luiz Henrique Santos <96305078+LuizHenriqueSantos15@users.noreply.github.com>
2022-02-16 17:17:17 -0300

    Add files via upload

---
+++ Controllers/FilmeController.cs
@@ -0,0 +1,54 @@
+﻿using LojaRoupaAPIWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaRoupaAPIWeb.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FilmeController : Controller
+    {
+        [HttpGet]
+
+        public ActionResult Get()
+        {
+            using (var contexto = new FilmeContext())
+            {
+                var filmes = contexto.Filmes.ToList();
+
+                return Ok(filmes);
+            }
+        }
+
+        [HttpGet("{produtora}")]
+
+        public ActionResult GetByProdutora(string produtora)
+        {
+            using (var contexto = new FilmeContext())
+            {
+                var filmes = (from s in contexto.Filmes where s.Produtora.Contains(produtora) select s).ToList();
+
+                //var filmes = contexto.Filmes.ToList();
+
+                return Ok(filmes);
+            }
+        }
+
+        [HttpGet("/api/[controller]/buscar/{filme}")]
+
+        public ActionResult GetByFilme(string filme)
+        {
+            using (var contexto = new FilmeContext())
+            {
+                var filmes = (from s in contexto.Filmes where s.Nome.Contains(filme) select s).ToList();
+
+                //var filmes = contexto.Filmes.ToList();
+
+                return Ok(filmes);
+            }
+        }
+    }
+}
